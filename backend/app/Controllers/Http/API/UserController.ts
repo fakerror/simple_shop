4e8ff8f2f4dea221144e61a0f286d81ad3f12423
fakerror <7368920+fakerror@users.noreply.github.com>
2021-12-18 22:15:19 +0800

    实现用户注册 改密API

--- backend/app/Controllers/Http/API/UserController.ts
+++ backend/app/Controllers/Http/API/UserController.ts
@@ -1,4 +1,5 @@
 import { Exception } from '@adonisjs/core/build/standalone'
+import Hash from '@ioc:Adonis/Core/Hash'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
@@ -31,11 +32,33 @@ export default class UserController {
       const token = await ctx.auth.use('api').attempt(username, password)
       return token
     } catch {
-      return ctx.response.badRequest('Invalid credentials')
+      ctx.response.badRequest('错误的用户名或者密码')
+      return
     }
   }
+  public async register(ctx: HttpContextContract) {
+    const username = ctx.request.input('username')
+    const password = ctx.request.input('password')
+
+    if (await User.findBy('username', username)) {
+      ctx.response.badRequest('用户名已存在')
+      return
+    }
+    const user = new User()
+    await user.fill({ username: username, password: password }).save()
+    return '注册成功'
+  }
+
   public async change_password(ctx: HttpContextContract) {
     const user = await ctx.auth.use('api').authenticate()
-    return user
+
+    const oldPassword = ctx.request.input('old_password')
+    const newPassword = ctx.request.input('new_password')
+    if (!(await Hash.verify(user.password, oldPassword))) {
+      ctx.response.badRequest('原密码不正确')
+      return
+    }
+    user.password = newPassword
+    await user.save()
   }
 }
