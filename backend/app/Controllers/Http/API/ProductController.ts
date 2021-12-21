0a63821364150e3e930b139b09c10d3b79db6c13
fakerror <7368920+fakerror@users.noreply.github.com>
2021-12-21 19:24:16 +0800

    新增product增改API

--- backend/app/Controllers/Http/API/ProductController.ts
+++ backend/app/Controllers/Http/API/ProductController.ts
@@ -1,4 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
 import Product from 'App/Models/Product'
 
 export default class ProductController {
@@ -10,8 +11,39 @@ export default class ProductController {
       }
       return await Product.find(id)
     }
-    async function post() {}
-    async function put() {}
+    async function post() {
+      const validator = schema.create({
+        name: schema.string({ trim: true }),
+        description: schema.string({ trim: true }),
+        value: schema.number(),
+        quantity: schema.number(),
+      })
+      const args = await ctx.request.validate({ schema: validator })
+
+      const product = new Product()
+      product.fill(args)
+      await product.save()
+    }
+    async function put() {
+      const validator = schema.create({
+        id: schema.string({ trim: true }),
+        name: schema.string({ trim: true }),
+        description: schema.string({ trim: true }),
+        value: schema.number(),
+        quantity: schema.number(),
+      })
+      const args = await ctx.request.validate({ schema: validator })
+
+      const product = await Product.find(args.id)
+      if (!product) {
+        return
+      }
+      product.name = args.name
+      product.description = args.description
+      product.value = args.value
+      product.quantity = args.quantity
+      await product.save()
+    }
     switch (ctx.request.method()) {
       case 'GET':
         return await get()
